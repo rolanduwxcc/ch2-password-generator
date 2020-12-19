94f9f66dc0db7bc5378ee5b611aca0dd3995df1c
rolanduwxcc <rolanduwxcc@gmail.com>
2020-12-19 16:11:32 -0600

    generate button works and can return password value

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,17 @@
 // Assignment code here
-
+var generatePassword = function() {
+  return "Bu-bye Cruel World!";
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+console.log("the value of generateBtn is ",generateBtn);
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  console.log("the value of passwordText is ",passwordText);
 
   passwordText.value = password;
 
