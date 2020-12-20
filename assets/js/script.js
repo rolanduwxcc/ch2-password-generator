22c28f1a86ef21436a5997950048ba48e7a35f32
rolanduwxcc <rolanduwxcc@gmail.com>
2020-12-19 20:59:33 -0600

    password generates as expected with proper input, no input safeguards yet

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,57 @@
-// ASCII Characters are
-// 65-90 uppercase letters
-// 48-57 numbers 0-9
-// 97-122 lowercase letters
-// the rest are special characters 33-126 is everything
+// ASCII Characters are being used for passwords, 33-126 is range of letters, numbers and 
+//special characters that will be used. 
+// 65-90 uppercase letters, 48-57 numbers 0-9, 97-122 lowercase letters
 
 var generatePassword = function() {
   var passwordString="";
-  // return "Bu-bye Cruel World!";
+
   window.alert("Please choose criteria options for password!");
   var length = window.prompt("Enter a number length between 8 and 128 of password: ");
   var useUpper = window.confirm("Should the password have upper case letters?");
-  var useLower = window.confirm("Should the password have lowerr case letters?");
+  var useLower = window.confirm("Should the password have lower case letters?");
+  var useNumeric = window.confirm("Should the password have numbers?");
   var useSpecial = window.confirm("Should the password have special characters (*, ! @ $, etc.)?");
   // return length + "|" + useUpper + "|" + useLower + "|" + useSpecial;
 
-  for (var i = 0; i < length; i++) {
+
+  while (passwordString.length < length) {
+    var isUpper = false; isLower = false; isNumeric = false; isSpecial = false;
     var charValue = Math.floor((Math.random() * 93) + 33);
     console.log(charValue);
-    // var useCharValue = false;
+    var useCharValue = false;
+    debugger;
 
-    // if (useUpper && (charValue >= 65 || charValue <=90)) {
-    //   generatedPW = generatedPW + String.fromCharCode(charValue);
-    // }
-    // else if (useLower && (charValue >= 97 || charValue <= 122)) {
-    //   generatedPW = generatedPW + String.fromCharCode(charValue);
-    // }
-    // else if (useSpecial)
-    passwordString = passwordString + String.fromCharCode(charValue);
+    if (charValue >= 65 && charValue <=90) {
+      isUpper = true; 
+    }
+    if (charValue >= 97 && charValue <= 122) {
+      isLower = true;
+    }
+    if (charValue >= 48 && charValue <= 57) {
+      isNumeric = true;
+    }
+    if (!isUpper && !isLower && !isNumeric){
+      isSpecial = true;
+    } 
+    
+    if (
+      (isUpper && useUpper) ||
+      (isLower && useLower) ||
+      (isNumeric && useNumeric) ||
+      (isSpecial && useSpecial)
+      ) {
+      useCharValue = true;
+    }
+
+    if (useCharValue) {
+      passwordString = passwordString + String.fromCharCode(charValue);
+    }
   }
   return passwordString;
-
-
-
 };
 
-// Get references to the #generate element
-// this things sets generateBtn to the whole html button element in the html, so that
-// at the bottom we add an event listener to listen for a click and that calls
+// Get references to the #generate element this thing sets generateBtn to the whole html button element 
+//in the html, so that at we can add an event listener to listen for a button click and that calls
 // the writePassword function!!!!
 var generateBtn = document.querySelector("#generate");
 console.log("the value of generateBtn is ",generateBtn);
@@ -48,7 +63,6 @@ function writePassword() {
   console.log("the value of passwordText is ",passwordText);
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
