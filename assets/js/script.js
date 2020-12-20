7dd7ba63311cd3480d15a15ac592189d6f4b93ea
rolanduwxcc <rolanduwxcc@gmail.com>
2020-12-19 19:32:01 -0600

    prompts for criteria and can gen a password using any ASCII Char from 33 to 126

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,43 @@
-// Assignment code here
+// ASCII Characters are
+// 65-90 uppercase letters
+// 48-57 numbers 0-9
+// 97-122 lowercase letters
+// the rest are special characters 33-126 is everything
+
 var generatePassword = function() {
-  return "Bu-bye Cruel World!";
+  var passwordString="";
+  // return "Bu-bye Cruel World!";
+  window.alert("Please choose criteria options for password!");
+  var length = window.prompt("Enter a number length between 8 and 128 of password: ");
+  var useUpper = window.confirm("Should the password have upper case letters?");
+  var useLower = window.confirm("Should the password have lowerr case letters?");
+  var useSpecial = window.confirm("Should the password have special characters (*, ! @ $, etc.)?");
+  // return length + "|" + useUpper + "|" + useLower + "|" + useSpecial;
+
+  for (var i = 0; i < length; i++) {
+    var charValue = Math.floor((Math.random() * 93) + 33);
+    console.log(charValue);
+    // var useCharValue = false;
+
+    // if (useUpper && (charValue >= 65 || charValue <=90)) {
+    //   generatedPW = generatedPW + String.fromCharCode(charValue);
+    // }
+    // else if (useLower && (charValue >= 97 || charValue <= 122)) {
+    //   generatedPW = generatedPW + String.fromCharCode(charValue);
+    // }
+    // else if (useSpecial)
+    passwordString = passwordString + String.fromCharCode(charValue);
+  }
+  return passwordString;
+
+
+
 };
 
 // Get references to the #generate element
+// this things sets generateBtn to the whole html button element in the html, so that
+// at the bottom we add an event listener to listen for a click and that calls
+// the writePassword function!!!!
 var generateBtn = document.querySelector("#generate");
 console.log("the value of generateBtn is ",generateBtn);
 
